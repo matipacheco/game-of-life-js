81e4f80c1cecbe035692f826f81afd7c5751a37d
Mati <matipacheco@gmail.com>
2018-09-18 13:00:44 -0300

    turururuuuu

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,8 @@
-/*
-* NOTA PARA EL FUTURO
-*
-* import {getRandomTerminalStates} from "./life_state_manager.js";
-* export function getRandomTerminalStates()....
-*
-* */
+/*******************************************************************************
+* UTILS
+*******************************************************************************/
 
-const dimension = 10;
+const cluster_dimension = 10, row = ".row_", col = ".col_";
 
 const COLORS = {
     0 : "black", // 0 : dead
@@ -15,84 +11,26 @@ const COLORS = {
     3 : "white"  // 3 : about to die
 };
 
-function getColor(state) {
+function get_color(state) {
     return COLORS[state];
 }
 
-
-
-class Cell {
-    constructor(row, col) {
-        this._row = row;
-        this._col = col;
-        this._state = getRandomTerminalStates();
-    }
-
-    get state() {
-        return this._state;
-    }
-
-/*    get cellPos() {
-        return this._row, this._col;
-    }*/
+function change_cell_color(cell_grid, cell) {
+    cell_grid.css("background-color", get_color(cell.state));
 }
 
+function populate_cluster() {
+    let cell, cell_grid, cluster = [];
 
-
-function getRandomTerminalStates() {
-    return getTerminalStates()[Math.floor(Math.random() * 2)];
-}
-
-function nextState(state) {
-    return (state + 1) % 4;
-}
-
-function aliveStates() {
-    return [2, 3];
-}
-
-function deadStates() {
-    return [0, 1];
-}
-
-function getTerminalStates() {
-    return [0, 2];
-}
-
-function getIntermediateStates() {
-    return [1, 3];
-}
-
-function cellIsAlive(cell) {
-    return aliveStates().includes(cell.state);
-}
-
-function cellIsDead(cell) {
-    return deadStates().includes(cell.state);
-}
-
-
-function changeCellColor(cell_grid, cell) {
-    cell_grid.css("background-color", getColor(cell.state));
-}
-
-
-
-function populateCluster(dimension) {
-    let cluster = [];
-
-    let cell, cell_grid;
-    const row = ".row_", col = ".col_";
-
-    for (let row_index = 1; row_index <= dimension; row_index++) {
+    for (let row_index = 0; row_index < cluster_dimension; row_index++) {
         let cluster_row = [];
 
-        for (let col_index = 1; col_index <= dimension; col_index++) {
-            cell = new Cell(row_index, col_index);
+        for (let col_index = 0; col_index < cluster_dimension; col_index++) {
+            cell = new Cell();
             cluster_row.push(cell);
 
             cell_grid = $(row + row_index).filter(col + col_index);
-            changeCellColor(cell_grid, cell);
+            change_cell_color(cell_grid, cell);
         }
 
         cluster.push(cluster_row);
@@ -101,12 +39,85 @@ function populateCluster(dimension) {
     return cluster;
 };
 
+function to_intermediate_states(cluster_grid) {
+    for (let row_index = 0; row_index < cluster_dimension; row_index++) {
+        for (let col_index = 0; col_index < cluster_dimension; col_index++) {
 
+            let cell = cluster_grid[row_index][col_index];
+            let current_state = cell.state;
+            cell.state = next_state(current_state);
+        }
+    }
+}
+
+
+
+/*******************************************************************************
+ * CELL
+ *******************************************************************************/
+
+class Cell {
+    constructor() {
+        this._state = get_random_terminal_states();
+    }
+
+    get state() {
+        return this._state;
+    }
+
+    set state(state) {
+        this._state = state;
+    }
+
+    get is_alive() {
+        return alive_states().includes(this._state);
+    }
+
+    get is_dead() {
+        return dead_states().includes(this._state);
+    }
+}
+
+
+
+/*******************************************************************************
+ * CELL
+ *******************************************************************************/
+
+function get_random_terminal_states() {
+    return terminal_states()[Math.floor(Math.random() * 2)];
+}
+
+function next_state(state) {
+    return (state + 1) % 4;
+}
+
+function alive_states() {
+    return [2, 3];
+}
+
+function dead_states() {
+    return [0, 1];
+}
+
+function terminal_states() {
+    return [0, 2];
+}
+
+function intermediate_states() {
+    return [1, 3];
+}
+
+
+
+/*******************************************************************************
+ * MAIN
+ *******************************************************************************/
 
 function main() {
-    let cluster_grid = populateCluster(dimension);
-
-    console.log(cluster_grid);
+    let cluster_grid = populate_cluster();
+    to_intermediate_states(cluster_grid);
+    console.log(cluster_grid[0][0]);
 }
 
 main();
