fa9eba7c2f0e97ecbe3791e7ef4ed8093f20d5f0
Mati <matipacheco@gmail.com>
2018-09-17 21:09:33 -0300

    making the cluster bigger

--- assets/main.js
+++ assets/main.js
@@ -10,13 +10,19 @@ const COLORS = { 0 : "black", 1 : "black", 2 : "white", 3 : "white" };
 
 
 class Cell {
-    constructor() {
+    constructor(row, col) {
+        this._row = row;
+        this._col = col;
         this._lifeState = getRandomTerminalStates();
     }
 
     get lifeState() {
         return this._lifeState;
     }
+
+/*    get cellPos() {
+        return this._row, this._col;
+    }*/
 }
 
 
@@ -43,19 +49,41 @@ function getIntermediateStates() {
 }
 
 
-function populateCluster() {
-    const row = ".row_", col = ".col_";
+function changeCellColor(cell_grid, cell) {
+    cell_grid.css("background-color", getColor(cell.lifeState));
+}
+
+
+function populateCluster(dimension) {
+    let cluster = [];
 
     let cell, cell_grid;
+    const row = ".row_", col = ".col_";
 
-    for (let row_index = 1; row_index <= 5; row_index++) {
-        for (let col_index = 1; col_index <= 5; col_index++) {
-            cell      = new Cell();
+    for (let row_index = 1; row_index <= dimension; row_index++) {
+        let cluster_row = [];
+
+        for (let col_index = 1; col_index <= dimension; col_index++) {
+            cell = new Cell(row_index, col_index);
+            cluster_row.push(cell);
+
             cell_grid = $(row + row_index).filter(col + col_index);
-            cell_grid.css("background-color", getColor(cell.lifeState));
+            changeCellColor(cell_grid, cell);
         }
 
+        cluster.push(cluster_row);
+
     }
+
+    return cluster;
 };
 
-populateCluster();
+
+function main() {
+    let cluster_grid = populateCluster(10);
+
+
+    console.log(cluster_grid);
+}
+
+main();
