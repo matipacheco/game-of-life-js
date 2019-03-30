3790a6a080b083048499c01401684ff29f59ddfb
Mati <matipacheco@gmail.com>
2019-03-29 22:03:39 -0300

    toMatrix method

---
+++ src/utils/Utils.js
@@ -0,0 +1,5 @@
+import { clusterDimension } from './Constants'
+
+export default function toMatrix(array, dimension = clusterDimension) {
+  array.reduce((rows, key, index) => (index % dimension === 0 ? rows.push([key]) : rows[rows.length-1].push(key)) && rows, []);
+}
