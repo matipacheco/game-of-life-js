73e117e96fa2570be7fc839771321a7488a2902f
Mati <matipacheco@gmail.com>
2019-03-29 21:28:38 -0300

    refactor on cell color depending its state

--- game-of-life/src/javascript/Cell.js
+++ game-of-life/src/javascript/Cell.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
 import '../css/Cell.css'
+import React, { Component } from 'react';
+
+import { deadStates } from '../utils/StateUtils'
 
 class Cell extends Component {
   render() {
     return (
       <div className='cell'
            key={ this.props.index }
-           style={{ backgroundColor: (this.props.state === 0 ? 'black' : 'white') }}>
+           style={{ backgroundColor: (deadStates().includes(this.props.state) ? 'black' : 'white') }}>
       </div>
     );
   }
