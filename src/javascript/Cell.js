0753c6f5a7bb7ef2a38ca7af996220f42eba5e07
Mati <matipacheco@gmail.com>
2019-03-30 22:05:22 -0300

    color change

--- src/javascript/Cell.js
+++ src/javascript/Cell.js
@@ -2,14 +2,16 @@ import '../css/Cell.css'
 import React, { Component } from 'react'
 
 import { deadStates } from '../utils/StateUtils'
-import { randomColor } from '../utils/Utils'
+
+// If you want more crazy version of the cluster, change _red_ for _randomColor()_
+// import { randomColor } from '../utils/Utils'
 
 class Cell extends Component {
   render() {
     return (
       <div className='cell'
            key={ this.props.index }
-           style={{ backgroundColor: (deadStates().includes(this.props.state) ? randomColor() : 'black') }}>
+           style={{ backgroundColor: (deadStates().includes(this.props.state) ? 'red' : 'black') }}>
       </div>
     );
   }
